0ce305f63ecbb3970b73de258e7f217a6e1bb5d2
madfour <873912205@qq.com>
2021-03-22 22:14:28 +0800

    headerAvatar

--- src/layouts/component/Header/BasicHeader.vue
+++ src/layouts/component/Header/BasicHeader.vue
@@ -5,12 +5,18 @@
       :type="collapsed ? 'menu-unfold' : 'menu-fold'"
       @click="() => (collapsed = !collapsed)"
     />
+    <!--    <header-avatar class="header-item" />-->
+    <a-row>
+      right
+    </a-row>
   </a-layout-header>
 </template>
 
 <script>
+import HeaderAvatar from './HeaderAvatar'
 export default {
-  name: 'BasicHead',
+  name: 'BasicHeader',
+  comments: { HeaderAvatar },
   data() {
     return {
       collapsed: false
---
+++ src/layouts/component/Header/HeaderAvatar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="title">Admin.Jc</div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderAvatar',
+  computed: {},
+  methods: {
+    logout() {
+      console.log('LOGO')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
